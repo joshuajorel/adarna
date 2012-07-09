fb90dfcdbd5aaecbf14438a9b76787f4473dca1d
justinmandac <justinmandac@gmail.com>
2012-07-09 18:39:19 +0800

    removed code related to Magnetometer

--- jni/com_Threading_ThreadActivity.c
+++ jni/com_Threading_ThreadActivity.c
@@ -12,36 +12,30 @@
 #define LOOPER_ID 1
 #define ACC_SAMP_FREQ 100
 #define GYR_SAMP_FREQ 100
-#define MAG_SAMP_FREQ 100
 #define BAR_SAMP_FREQ 5
 
 
 int64_t lastAccTime = 0;
 int64_t lastGyrTime = 0;
-int64_t lastMagTime = 0;
 int64_t lastBarTime = 0;
 int64_t lastGpsTime = 0;
 
 int64_t accT = 0;
 int64_t gyrT = 0;
-int64_t magT = 0;
 int64_t barT = 0;
 int64_t gpsT = 0;
 
 struct timespec accTime;
 struct timespec gyrTime;
-struct timespec magTime;
 struct timespec barTime;
 struct timespec gpsTime;
 
 FILE* accF;
 FILE* gyrF;
-FILE* magF;
 FILE* barF;
 
 int accFlag = 0;
 int gyrFlag = 0;
-int magFlag = 0;
 int barFlag = 0;
 int stopFlag = 0;
 
@@ -53,32 +47,26 @@ double altC = 0;
 float accuracyC = 0;
 void *acc_data;
 void *gyr_data;
-void *mag_data;
 void *bar_data;
 
 const ASensor *accSensor;
 const ASensor *gyrSensor;
-const ASensor *magSensor;
 const ASensor *barSensor;
 
 ASensorEventQueue* accQueue;
 ASensorEventQueue* gyrQueue;
-ASensorEventQueue* magQueue;
 ASensorEventQueue* barQueue;
 
 static int get_acc_events(int fd, int events, void* data);
 static int get_gyr_events(int fd, int events, void* data);
-static int get_mag_events(int fd, int events, void* data);
 static int get_bar_events(int fd, int events, void* data);
 
 ASensorManager* accSensorManager;
 ASensorManager* gyrSensorManager;
-ASensorManager* magSensorManager;
 ASensorManager* barSensorManager;
 
 ALooper* accLooper;
 ALooper* gyrLooper;
-ALooper* magLooper;
 ALooper* barLooper;
 
 static pthread_mutex_t mutex;
@@ -110,17 +98,14 @@ JNIEXPORT void JNICALL Java_com_Threading_ThreadActivity_threader
 	pthread_create(&thread, NULL, input_gps, NULL);
 	acc_data = malloc(1000);
 	gyr_data = malloc(1000);
-	mag_data = malloc(1000);
 	bar_data = malloc(1000);
 
 	accF = fopen("/sdcard/acc-samp.csv", "w");
 	gyrF = fopen("/sdcard/gyr-samp.csv", "w");
-	magF = fopen("/sdcard/mag-samp.csv", "w");
 	barF = fopen("/sdcard/bar-samp.csv", "w");
 
 	fclose(accF);
 	fclose(gyrF);
-	fclose(magF);
 	fclose(barF);
 
 //	if (accF == NULL) {
@@ -143,7 +128,6 @@ JNIEXPORT void JNICALL Java_com_Threading_ThreadActivity_threader
 
 	accLooper = ALooper_forThread();
 	gyrLooper = ALooper_forThread();
-	magLooper = ALooper_forThread();
 	barLooper = ALooper_forThread();
 
 	if(accLooper == NULL){
@@ -154,48 +138,37 @@ JNIEXPORT void JNICALL Java_com_Threading_ThreadActivity_threader
 		gyrLooper = ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS);
 	}
 
-	if(magLooper == NULL){
-		magLooper = ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS);
-	}
-
 	if(barLooper == NULL){
 		barLooper = ALooper_prepare(ALOOPER_PREPARE_ALLOW_NON_CALLBACKS);
 	}
 
 	accSensorManager = ASensorManager_getInstance();
 	gyrSensorManager = ASensorManager_getInstance();
-	magSensorManager = ASensorManager_getInstance();
 	barSensorManager = ASensorManager_getInstance();
 
 	accSensor = ASensorManager_getDefaultSensor(accSensorManager, 10);
 	gyrSensor = ASensorManager_getDefaultSensor(gyrSensorManager, ASENSOR_TYPE_GYROSCOPE);
-	magSensor = ASensorManager_getDefaultSensor(magSensorManager, ASENSOR_TYPE_MAGNETIC_FIELD);
 	barSensor = ASensorManager_getDefaultSensor(barSensorManager, 6);
 
 	accQueue = ASensorManager_createEventQueue(accSensorManager, accLooper, 3, get_acc_events, acc_data);
 	gyrQueue = ASensorManager_createEventQueue(gyrSensorManager, gyrLooper, 3, get_gyr_events, gyr_data);
-	magQueue = ASensorManager_createEventQueue(magSensorManager, magLooper, 3, get_mag_events, mag_data);
 	barQueue = ASensorManager_createEventQueue(barSensorManager, barLooper, 3, get_bar_events, bar_data);
 
 	ASensorEventQueue_enableSensor(accQueue, accSensor);
 	ASensorEventQueue_enableSensor(gyrQueue, gyrSensor);
-	ASensorEventQueue_enableSensor(magQueue, magSensor);
 	ASensorEventQueue_enableSensor(barQueue, barSensor);
 
 	int a = ASensor_getMinDelay(accSensor);
 	int b = ASensor_getMinDelay(gyrSensor);
-	int c = ASensor_getMinDelay(magSensor);
 	int d = ASensor_getMinDelay(barSensor);
 
 	LOGI("min-delay: %d", a);
 	LOGI("min-delay: %d", b);
-	LOGI("min-delay: %d", c);
 	LOGI("min-delay: %d", d);
 
 
 	ASensorEventQueue_setEventRate(accQueue, accSensor, (1000L/ACC_SAMP_FREQ)*1000);
 	ASensorEventQueue_setEventRate(gyrQueue, gyrSensor, (1000L/GYR_SAMP_FREQ)*1000);
-	ASensorEventQueue_setEventRate(magQueue, magSensor, (1000L/MAG_SAMP_FREQ)*1000);
 	ASensorEventQueue_setEventRate(barQueue, barSensor, (1000L/BAR_SAMP_FREQ)*1000);
 
 	LOGI("sensorValue() - Start");
@@ -260,36 +233,6 @@ static int get_gyr_events(int fd, int events, void* data){
 	}
 	return 1;
 }
-
-static int get_mag_events(int fd, int events, void* data){
-	ASensorEvent event;
-	while(ASensorEventQueue_getEvents(magQueue, &event, 1) >0 && !stopFlag){
-		if(event.type == ASENSOR_TYPE_MAGNETIC_FIELD){
-			//LOGI("[%d][%f][%f][%f][%f]", gyrSampleCount, ((double)(event.timestamp-lastGyrTime))/1000000000.0, event.acceleration.x,event.acceleration.y,event.acceleration.z);
-			magF = fopen("/sdcard/mag-samp.csv", "a");
-			fprintf(magF, "%lld; %f; %f; %f; %f;\n", event.timestamp, ((double)(event.timestamp-lastMagTime))/1000000000.0, event.magnetic.x,event.magnetic.y,event.magnetic.z);
-			lastMagTime = event.timestamp;
-			fclose(magF);
-		}
-	}
-
-	if(stopFlag){
-		LOGI("MAG SAMPLING FINISHED");
-		magFlag = 1;
-		ASensorEventQueue_disableSensor(magQueue, magSensor);
-		LOGI("Disabled mag Sensor");
-		ASensorManager_destroyEventQueue(magSensorManager, magQueue);
-		LOGI("Freed mag manager");
-//		ALooper_release(magLooper);
-//		LOGI("Freed mag looper");
-		free(mag_data);
-		LOGI("Freed mag data");
-		fclose(magF);
-	}
-
-	return 1;
-}
-
 static int get_bar_events(int fd, int events, void* data){
 	ASensorEvent event;
 	while(ASensorEventQueue_getEvents(barQueue, &event, 1) >0 && !stopFlag){
